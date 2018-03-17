71bad2760a409bbaec6a021d8832f6dd0e334d53
wb-gs313456 <wb-gs313456@alibaba-inc.com>
2018-03-17 11:30:03 +0800

    add page

--- src/main/java/com/baijia/warehouse/app/WarehouseApp.java
+++ src/main/java/com/baijia/warehouse/app/WarehouseApp.java
@@ -9,13 +9,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author gushu
  * @date 2018/03/16
  */
-@SpringBootApplication(scanBasePackages={"com.baijia.warehouse.service"})
+@SpringBootApplication(scanBasePackages = { "com.baijia.warehouse.service" })
 @ImportResource(locations = { "classpath:mybatis/mybatis-spring.xml", "classpath:spring-conf/spring-beans.xml" })
 @Controller
 public class WarehouseApp {
@@ -23,10 +24,18 @@ public class WarehouseApp {
 	public static void main(String[] args) {
 		SpringApplication.run(WarehouseApp.class, args);
 	}
-	
+
 	@Resource(name = "warehouseService")
 	private WarehouseService warehouseService;
-	
+
+	@RequestMapping("/index")
+	public String hello(ModelMap modelMap) {
+		// 向模板中添加属性
+		modelMap.put("hello", "lokia");
+		// return模板文件的名称，对应src/main/resources/templates/index.html
+		return "index";
+	}
+
 	@RequestMapping("/add")
 	public ResponseResult add(StorageUnitDTO goodsDTO) {
 		warehouseService.add(goodsDTO);
